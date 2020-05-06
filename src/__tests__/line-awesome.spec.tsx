2719a8a20e722df8c7582e5cd8b0367011fb7305
William Sena <williampsena@gmail.com>
2020-05-06 08:52:02 -0300

    Add export

--- src/__tests__/line-awesome.spec.tsx
+++ src/__tests__/line-awesome.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { create } from 'react-test-renderer'
 import { LineAwesome } from '../components/line-awesome'
-import SvgThumbsUp from '../icons/ThumbsUp'
+import { SvgThumbsUp } from '../icons'
 
 describe('LineAwesome component', () => {
     test('Matches the snapshot"', () => {
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './components/line-awesome'
+export * from './icons'
